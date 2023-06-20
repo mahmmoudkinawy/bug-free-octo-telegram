e51cafd264a89453457bec67f918225729f8576c
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2023-06-20 13:06:27 +0300

    delete and get all deletgates are now working

--- src/app/pages/supervisors/supervisor-home/supervisor-home.component.ts
+++ src/app/pages/supervisors/supervisor-home/supervisor-home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AddDelegate } from 'src/app/models/add-delegate';
+import { Delegate } from 'src/app/models/delegate';
 import { SupervisorService } from 'src/app/services/supervisor.service';
 
 @Component({
@@ -9,7 +10,8 @@ import { SupervisorService } from 'src/app/services/supervisor.service';
   templateUrl: './supervisor-home.component.html',
   styleUrls: ['./supervisor-home.component.css'],
 })
-export class SupervisorHomeComponent {
+export class SupervisorHomeComponent implements OnInit {
+  delegates: Delegate[] = [];
   newDelegate: AddDelegate = {
     email: '',
     firstName: '',
@@ -24,6 +26,35 @@ export class SupervisorHomeComponent {
     private router: Router
   ) {}
 
+  ngOnInit(): void {
+    this.loadDelegates();
+  }
+
+  onDeleteDelegate(delegate: Delegate): void {
+    const confirmation = window.confirm(
+      'Do you really want to remove this delegate?'
+    );
+    if (confirmation) {
+      this.supervisorService.deleteDelegate(delegate).subscribe(
+        () => {
+          this.toastr.success('Delegate deleted successfully.');
+          this.loadDelegates();
+        },
+        (error) => {
+          this.toastr.success('Delegate deleted successfully.');
+          this.loadDelegates();
+          console.log(error);
+        }
+      );
+    }
+  }
+
+  loadDelegates() {
+    this.supervisorService
+      .loadDelegates()
+      .subscribe((delegates) => (this.delegates = delegates));
+  }
+
   onAddDelegate() {
     this.supervisorService
       .addDelegate(this.newDelegate, this.newDelegate.userImg!)
@@ -31,7 +62,8 @@ export class SupervisorHomeComponent {
         (response) => {
           console.log(response);
           this.toastr.success('Delegate added successfully');
-          this.router.navigateByUrl('/');
+          // this.router.navigateByUrl('/');
+          this.loadDelegates();
         },
         (error) => {
           this.toastr.error('Email or Phone already taken.');
--- src/app/services/supervisor.service.ts
+++ src/app/services/supervisor.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AddDelegate } from '../models/add-delegate';
 import { DelegateResponse } from '../models/delegate-response';
+import { Delegate, Delegates } from '../models/delegate';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +12,16 @@ export class SupervisorService {
   private readonly apiUrl = 'http://localhost:8080/supervisor';
 
   constructor(private http: HttpClient) {}
+
+  deleteDelegate(delegate: Delegate) {
+    return this.http.delete(`${this.apiUrl}/remove_delegate/${delegate._id}`);
+  }
+
+  loadDelegates() {
+    return this.http
+      .get<Delegates>(`${this.apiUrl}/delegates`)
+      .pipe(map((res) => res.delegates));
+  }
 
   addDelegate(delegate: AddDelegate, image: File) {
     const formData = new FormData();
